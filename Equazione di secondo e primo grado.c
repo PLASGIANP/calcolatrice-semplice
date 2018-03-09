17f45b4b4929d13ec103114874f09ab2c15689f8
PLASGIANP <33983040+PLASGIANP@users.noreply.github.com>
2018-03-09 18:44:08 +0100

    Update Equazione di secondo e primo grado.c

--- Equazione di secondo e primo grado.c
+++ Equazione di secondo e primo grado.c
@@ -18,32 +18,31 @@ int main (void)
 				printf("Questa e' u ma essendo a=0 diventa di primo grado\n");
 				printf("Inserire a e b");
 				scanf("%f\n%f",&a,&b);
-		        printf("Il risultato di questa equazione di primo grado e':");
-		        x1=-c/b;
-		        printf("%f",x1);
-	        	break;
-	        case 2:
-	        	printf("Inserisci i tre numeri a,b,c\n");
+		       		printf("Il risultato di questa equazione di primo grado e':");
+		        	x1=-c/b;
+		        	printf("%f",x1);
+	        		break;
+	        	case 2:
+	        		printf("Inserisci i tre numeri a,b,c\n");
 	  			scanf("%f%f%f",&a,&b,&c);
-	        	printf("L'equazione e' completa quindi:\n");
-	      		delta=(b*b-4*a*c);
-	     		 if (delta >= 0)
-	     		 {
+	        		printf("L'equazione e' completa quindi:\n");
+	      			delta=(b*b-4*a*c);
+	     			 if (delta >= 0)
+	     			 {
 	          			x1=(-b+sqrt(delta))/(2*a);
 	          			x2=(-b-sqrt(delta))/(2*a);
 	          			printf("I valori di x1 e x2 sono:%f e %f",x1,x2);
-	      		 }
-	     		 if (delta < 0)
-	     		 {
+	      			 }
+	     		 	if (delta < 0)
+	     		 	{
 	          			printf("Il delta e' minore di 0 quindi il risultato coincide con l'insieme dei numeri reali R");
-	      		 }
-	      		 default:
+	      		 	}
+				break;
+	      		default:
 	      		 	printf("Uscita dal programma");
-	      		 	return 0;
 	      		break;
 	   	}
-	   	
-   	}while((n>4) || (n<0));
+	 }while((n>4) || (n<0));
    	
    	return 0;
 }
